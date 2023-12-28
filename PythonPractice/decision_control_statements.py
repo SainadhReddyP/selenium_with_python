5c4aaeb6da2f22b6f627c81343c0e07751fe305d
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-28 22:49:04 +0530

    control statements example

---
+++ PythonPractice/decision_control_statements.py
@@ -0,0 +1,11 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+if num1>num2:
+    print("{} is greater than {}".format(num1,num2))
+elif num1<num2:
+    print("{} is greater than {}".format(num2,num1))
+else:
+    print("{} is equal to {}".format(num2,num1))
+
+
