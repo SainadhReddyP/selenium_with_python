22fe4d3ecee7a03dd0d22b07614d09562213678a
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 11:24:25 +0530

    function

---
+++ PythonPractice/funtions_example.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("Function is a block of code.")
+
+
+def print_name(name):
+    print("Your name is: "+name)
+
+# function calling statements
+my_function()
+print_name("Sainadh")
