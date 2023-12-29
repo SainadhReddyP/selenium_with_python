14f753580f203bf71d787dfd9f54346856830ea2
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 11:32:41 +0530

    default argument function

--- PythonPractice/funtions_example.py
+++ PythonPractice/funtions_example.py
@@ -5,6 +5,13 @@ def my_function():
 def print_name(name):
     print("Your name is: "+name)
 
+
+def default_argument_function(name='Sainadh'):
+    print("My name is: "+name)
+
+
 # function calling statements
 my_function()
 print_name("Sainadh")
+default_argument_function()
+default_argument_function("Reddy")
