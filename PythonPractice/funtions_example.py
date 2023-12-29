3be941eda7f52fc2ccd93285e05e9c8ed8a59e84
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 11:50:34 +0530

    functions

--- PythonPractice/funtions_example.py
+++ PythonPractice/funtions_example.py
@@ -16,8 +16,24 @@ def default_argument_function(name='Sainadh'):
     print("My name is: "+name)
 
 
+def multiple_parameters_function(a,b,c):
+    """
+    Description: function can contains any number of parameters
+    :return:
+    """
+    result_sum = a + b + c
+    print("The sum of "+str(a)+", "+str(b)+", "+str(c)+" is: "+str(result_sum))
+
+
+def sum_of_three_numbers(num1,num2,num3):
+    return num1+num2+num3
+
 # function calling statements
 my_function()
 print_name("Sainadh")
 default_argument_function()
 default_argument_function("Reddy")
+multiple_parameters_function(1,2,3)
+result = sum_of_three_numbers(5,4,3)
+print(result)
+
