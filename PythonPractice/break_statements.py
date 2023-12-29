8110de5754f3741798e47bf4a71c0daa32b0d1e4
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 11:01:12 +0530

    break and continue

---
+++ PythonPractice/break_statements.py
@@ -0,0 +1,5 @@
+
+for i in range(1,7):
+    if i == 3:
+        break # it will cancel the iteration after this
+    print(i)
---
+++ PythonPractice/continue_statements.py
@@ -0,0 +1,5 @@
+
+for i in range(1,7):
+    if i == 3:
+        continue # it will cancel current iteration
+    print(i)
