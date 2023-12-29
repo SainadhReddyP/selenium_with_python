178cbc00c5df5567781fc4db3a11b15115133bc5
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 23:05:01 +0530

    collections-list

--- PythonPractice/collections_list.py
+++ PythonPractice/collections_list.py
@@ -3,3 +3,17 @@ print(my_list)
 
 # indexing
 print(my_list[2])
+print(my_list[0])
+
+# length of list
+print(len(my_list))
+
+# for each loop
+for i in my_list:
+    print(i)
+
+# for loop with range function
+for i in range(len(my_list)):
+    print(my_list[i])
+
+
