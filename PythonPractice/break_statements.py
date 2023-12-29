f61cc93eda4098314597d32e3025aa0308f2b462
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 11:09:36 +0530

    function

--- PythonPractice/break_statements.py
+++ PythonPractice/break_statements.py
@@ -3,3 +3,11 @@ for i in range(1,7):
     if i == 3:
         break # it will cancel the iteration after this
     print(i)
+
+print("While-break")
+j = 1
+while j <= 7:
+    if j == 5:
+        break
+    print(j)
+    j = j+1
--- PythonPractice/continue_statements.py
+++ PythonPractice/continue_statements.py
@@ -3,3 +3,16 @@ for i in range(1,7):
     if i == 3:
         continue # it will cancel current iteration
     print(i)
+
+print("While-continue")
+
+j = 1
+
+while j <= 7:
+    if j == 3:
+        j = j+1 # without this the j won't be incremented.
+        continue
+    print(j)
+    j = j+1
+
+
