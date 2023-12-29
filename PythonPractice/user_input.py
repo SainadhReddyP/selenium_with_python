800869cf94922ecd5421dc1cb05ae2d3896b653c
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 12:37:45 +0530

    user input in-built function

---
+++ PythonPractice/user_input.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+print("Welcome "+name)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+addition = num1+num2
+
+print("Sum of two numbers is: "+str(addition))
