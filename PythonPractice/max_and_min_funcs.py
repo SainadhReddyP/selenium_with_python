daaccf87b6df568a806218985df4053e21eb0818
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-29 12:47:36 +0530

    max and min functions

---
+++ PythonPractice/max_and_min_funcs.py
@@ -0,0 +1,14 @@
+
+max_value = max(51,32,334,45,67,90)
+min_value = min(51,32,334,45,67,90)
+
+print("Maximum value is:",max_value)
+print("Minimum value is:",min_value)
+
+# with string text
+
+max_text = max("python","java","javascript","typescript","golang") # t comes last among all
+min_text = min("python","java","javascript","typescript","golang") # g comes first among all
+
+print("Maximum text is:",max_text)
+print("Minimum text is:",min_text)
