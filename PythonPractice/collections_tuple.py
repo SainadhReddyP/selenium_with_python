156a26eb95177d9a0ac3c2a4fcf57d9add892b91
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-30 12:39:57 +0530

    collections-tuple

---
+++ PythonPractice/collections_tuple.py
@@ -0,0 +1,9 @@
+my_tuple = (1,2,3,4,5,6,7)
+
+print(my_tuple)
+
+# my_tuple[2] = 8 # TypeError: 'tuple' object does not support item assignment
+# we cannot update the tuple
+
+# access values in tuple
+
