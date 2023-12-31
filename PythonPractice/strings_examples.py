8f4d3410828fb56635605fe85413a4c1e84713db
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-31 08:43:13 +0530

    strings examples

--- PythonPractice/strings_examples.py
+++ PythonPractice/strings_examples.py
@@ -11,6 +11,31 @@ print(type(first_name))
 print(type(last_name))
 print(type(location))
 
+# strings work like collections
+print(first_name[0]) # Indexing is possible
+print(len(first_name)) # finding length(size)
+
+# for each loop
+for char in first_name:
+    print(char)
+
+# for loop with range
+for c in range(len(first_name)):
+    print(first_name[c],end="")
+print("\n")
+
+# in and not in operators
+print("Sai" in first_name)
+
+# slicing
+print(first_name[1:3])
+
+#
+
+
+
+
+
 
 
 
