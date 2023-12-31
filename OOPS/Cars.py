02c6f65971bcac0d0cf51b87c2a716821de8ca0e
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-31 12:11:56 +0530

    OOPS-example

---
+++ OOPS/Cars.py
@@ -0,0 +1,15 @@
+# Function - Function outside the class is known as Function.
+def sample_function():
+    print("Inside sample function.")
+
+
+class Cars:
+    wheels = 4
+
+    # Method - Function inside the class is known as Method.
+    def start_car(self):
+        print("Car started.")
+
+
+# Object creation statement by using Class name
+Cars()
