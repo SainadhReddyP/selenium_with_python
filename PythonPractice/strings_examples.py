bc0cef79b1a6ec80ee04b779e0a47089518ee0f8
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-31 08:37:45 +0530

    strings

---
+++ PythonPractice/strings_examples.py
@@ -0,0 +1,19 @@
+# creating strings
+first_name = "Sainadh"
+last_name = 'Reddy'
+location = str("Hyderabad")
+
+print(first_name)
+print(last_name)
+print(location)
+
+print(type(first_name))
+print(type(last_name))
+print(type(location))
+
+
+
+
+
+
+
