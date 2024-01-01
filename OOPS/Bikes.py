e63efa71bcd1ebf121cb2ebede0d29c50151719f
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-01 15:39:22 +0530

    class bikes example

---
+++ OOPS/Bikes.py
@@ -0,0 +1,28 @@
+# Initializing Class variables using methods
+
+class Bikes:
+
+    def initialization_method(self,brand,model,price,mileage):
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.mileage = mileage
+        
+    def start_bike(self):
+        print(self.brand+" bike having model as "+self.model+" has started")
+
+    def stop_bike(self):
+        print(self.brand+" bike having model as "+self.model+" has stopped")
+        
+    def print_bike_details(self):
+        print("Brand of the bike is: "+self.brand)
+        print("Model of the bike is: "+self.model)
+        print("Price of the bike is: "+str(self.price))
+        print("Mileage of the bike is: "+str(self.mileage))
+        
+
+shine = Bikes()
+shine.initialization_method('Honda','CB Shine',85000,54.5)
+shine.start_bike()
+shine.stop_bike()
+shine.print_bike_details()
