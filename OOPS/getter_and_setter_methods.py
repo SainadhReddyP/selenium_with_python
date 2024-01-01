f11a2b37cb89eeefbbfd170809924e5d73cbd3b6
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-01 20:47:02 +0530

    getter and setter methods

--- OOPS/getter_and_setter_methods.py
+++ OOPS/getter_and_setter_methods.py
@@ -3,5 +3,20 @@ Using Getter and Setter methods with private variables
 - Getter and Setter non-private methods can be used for accessing the private variables.
 """
 
+class A:
+
+    __age = 0  # private variable - can only be accessible inside the class
+
+    def set_age(self,age):
+        A.__age = age
+
+    def get_age(self):
+        return A.__age
+
+obj = A()
+# print(obj.__age)  # cannot access private variable
+
+obj.set_age(28)
+print(obj.get_age())
 
 
