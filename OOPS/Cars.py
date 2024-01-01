cb536e439cb2e5620542f030200d99a4f2be463f
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-01 12:07:26 +0530

    self keyword

--- OOPS/Cars.py
+++ OOPS/Cars.py
@@ -13,6 +13,27 @@ class Cars:
     def stop_car(self):
         print("Car stopped.")
 
+    def sample_one(self,brand,model,price,mileage):
+        print(brand,model,price,mileage)
+
+    def sample_two(self):
+        print(self.wheels)
+
+    def sample_three(self,brand,model,price,mileage):
+        # to access properties of method outside the method
+        # by creating class variables - we can access
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.mileage = mileage
+        print(brand,model,price,mileage)
+
+    def sample_four(self):
+        print(self.wheels)
+        # we can access the other method variables outside the method
+        print(self.brand,self.model,self.price,self.mileage)
+        self.start_car()
+
 
 # Object creation statement by using Class name
 obj = Cars()
@@ -28,3 +49,9 @@ print("Honda Brio.")
 brio = Cars()
 brio.start_car()
 
+
+new_obj = Cars()
+new_obj.sample_one("Honda","Brio",800000,19.5)
+new_obj.sample_two()
+new_obj.sample_three("Maruti","Swift",900000,17)
+new_obj.sample_four()
