2006a0b6bfab887b480e867f3d0d97f9a9ac32f5
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-03 22:38:10 +0530

    modules-examples

--- PythonPractice/modules_in_python.py
+++ PythonPractice/modules_in_python.py
@@ -2,3 +2,22 @@
 Module is a python file containing a set of functions.
 """
 
+
+def addition(a, b):
+    sum = a + b
+    return sum
+
+
+def subtraction(a, b):
+    sub = a - b
+    return sub
+
+
+def multiplication(a, b):
+    mul = a * b
+    return mul
+
+
+def divison(a, b):
+    div = a / b
+    return div
---
+++ PythonPractice/modules_in_python_example.py
@@ -0,0 +1,37 @@
+from PythonPractice import modules_in_python
+from PythonPractice.modules_in_python import addition,subtraction,multiplication,divison
+from PythonPractice.modules_in_python import *
+
+# Approach 1
+r1 = modules_in_python.addition(4,5)
+print(r1)
+
+
+r2 = modules_in_python.subtraction(5,4)
+print(r2)
+
+
+r3 = modules_in_python.multiplication(4,5)
+print(r3)
+
+
+r4 = modules_in_python.divison(5,4)
+print(r4)
+
+
+# Approach 2
+r1 = addition(4,5)
+print(r1)
+
+
+r2 = subtraction(5,4)
+print(r2)
+
+
+r3 = multiplication(4,5)
+print(r3)
+
+
+r4 = divison(5,4)
+print(r4)
+
