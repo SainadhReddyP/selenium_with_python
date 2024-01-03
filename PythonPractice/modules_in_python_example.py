b5b42838dcf0f4430c6a18332d8738afdb0b9e82
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-03 22:55:48 +0530

    random module - inbuilt module

--- PythonPractice/modules_in_python_example.py
+++ PythonPractice/modules_in_python_example.py
@@ -1,37 +1,33 @@
 from PythonPractice import modules_in_python
-from PythonPractice.modules_in_python import addition,subtraction,multiplication,divison
+from PythonPractice.modules_in_python import addition, subtraction, multiplication, divison
 from PythonPractice.modules_in_python import *
 
+print(dir(modules_in_python))  # This will return set of functions in the module
+
 # Approach 1
-r1 = modules_in_python.addition(4,5)
+r1 = modules_in_python.addition(4, 5)
 print(r1)
 
-
-r2 = modules_in_python.subtraction(5,4)
+r2 = modules_in_python.subtraction(5, 4)
 print(r2)
 
-
-r3 = modules_in_python.multiplication(4,5)
+r3 = modules_in_python.multiplication(4, 5)
 print(r3)
 
-
-r4 = modules_in_python.divison(5,4)
+r4 = modules_in_python.divison(5, 4)
 print(r4)
-
 
 # Approach 2
-r1 = addition(4,5)
+r1 = addition(4, 5)
 print(r1)
 
-
-r2 = subtraction(5,4)
+r2 = subtraction(5, 4)
 print(r2)
 
-
-r3 = multiplication(4,5)
+r3 = multiplication(4, 5)
 print(r3)
 
-
-r4 = divison(5,4)
+r4 = divison(5, 4)
 print(r4)
+
 
---
+++ PythonPractice/random_example.py
@@ -0,0 +1,9 @@
+import random
+
+colors = ['orange','green','blue','white','yellow','violet']
+
+print(random.choice(colors))
+
+# It will return a new number every time - random value
+number = random.randint(1,100)
+print(number)
