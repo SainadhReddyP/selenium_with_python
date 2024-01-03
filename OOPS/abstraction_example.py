4f841c42f49b971a797986e09d83b1a4c743abf6
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-03 22:27:13 +0530

    abstraction-example

--- OOPS/abstraction_example.py
+++ OOPS/abstraction_example.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 """
 Abstract Class - which has to become child class of predefined parent Abstract class ABC.
@@ -9,15 +9,43 @@ Abstract Class-  partial implementation in it (Some are Abstract methods (doesn'
 
 class A(ABC):  # ABC is predefined parent abstract class
 
+    def __init__(self, a):
+        self.a = a
+
     # abstract methods - which does not have body in it.
     # Abstract method
+    @abstractmethod
     def method_one(self):
         pass
 
     # Abstract method
+    @abstractmethod
     def method_two(self):
         pass
 
     # Non-Abstract method
     def method_three(self):
-        print("Inside methode three.")
+        print("Inside methode three.",self.a)
+
+
+class B(A):  # Child Class of Parent Abstract Class
+
+    # Implement all the abstract methods of parent abstract class
+    def method_one(self):
+        print("Inside method_one.")
+
+    def method_two(self):
+        print("Inside method_two.")
+
+
+# TypeError:
+# Can't instantiate abstract class A with abstract methods method_one, method_two
+
+# obj = A()
+# we can create child class and can create object for it
+
+obj_b = B(5)
+obj_b.method_one()
+obj_b.method_two()
+obj_b.method_three()
+
