46d7caa150cd74cdb28b5bca9b0840f6b95869ff
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-04 10:39:28 +0530

    lambda functions

---
+++ PythonPractice/lambda_functions.py
@@ -0,0 +1,10 @@
+"""
+Lambda Function:
+- A function which has no name, and can intake any number of arguments
+and one expression is Lambda function.
+- Lambda function reduces the number of lines of code to be written in a normal function.
+"""
+
+sum = lambda a,b : a+b
+print(sum(5,4))
+
