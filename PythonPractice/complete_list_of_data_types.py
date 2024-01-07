8bfbade6c877a23b583bc156017bcaf42a270d0f
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 19:22:44 +0530

    complete_list_of_data_types.py

---
+++ PythonPractice/complete_list_of_data_types.py
@@ -0,0 +1,11 @@
+"""
+The following is the complete list of data types in Python:
+Text Type - str
+Numeric Type - int, float and complex
+Sequence Type - list, tuple and range
+Mapping Type - dict
+Set Type - set and frozenset
+Boolean Type - bool
+Binary Type - bytes, bytearray and memoryview
+"""
+
