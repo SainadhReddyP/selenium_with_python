8993ffa35d149c9c98c49c7472933fd2d84c2874
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 22:43:12 +0530

    escape characters

---
+++ PythonPractice/escape_characters.py
@@ -0,0 +1,15 @@
+"""
+- Escape characters are used in String text for inserting illegal characters,
+tab space, new line and many more.
+Examples:
+\'
+\"
+\\
+\n - new line
+\t - tab space
+\r - carriage return (removes the before part)
+\b - backspace
+"""
+my_string = 'My name is \'Sainadh\' Reddy.'
+print(my_string)
+
