a9740591d65d3d9cb6442c9a74fbcac175fcd0c1
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 19:28:40 +0530

    complete_list_of_data_types.py

--- PythonPractice/complete_list_of_data_types.py
+++ PythonPractice/complete_list_of_data_types.py
@@ -7,5 +7,18 @@ Mapping Type - dict
 Set Type - set and frozenset
 Boolean Type - bool
 Binary Type - bytes, bytearray and memoryview
+
+set - mutable
+forzenset - immutable
 """
+
+my_set = {1,3,5,7}
+print(type(my_set))
+my_set.add(9)
+print(my_set)
+
+my_frozen_set = frozenset(my_set)
+print(type(my_frozen_set))
+
+my_frozen_set.add(11)  # we cannot update it - frozenset is immutable
 
