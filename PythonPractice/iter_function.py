1e7e193dba5eb83885d621881683f5e0bb2b978d
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 22:54:45 +0530

    iter function

---
+++ PythonPractice/iter_function.py
@@ -0,0 +1,11 @@
+
+my_list = [1,2,3,4,5]  # same for tuple, set, dictionary
+my_dict = {'name':'Sainadh','age':28,'location':'Hyderabad'}
+
+iterator = iter(my_list)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
