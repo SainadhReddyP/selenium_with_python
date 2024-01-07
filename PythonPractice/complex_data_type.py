a988e1809af293cbf20021d8428f4d6b7d11c773
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 11:22:13 +0530

    complex data type

--- PythonPractice/complex_data_type.py
+++ PythonPractice/complex_data_type.py
@@ -8,12 +8,23 @@ j - means -> square root of -1
 example:
 3+5j
 
+
+-> Real value of complex data type is optional
+
+c = 10j
 """
 c = 3+5j
+d = 1+4j
+e = 10j
 print(type(c))  # <class 'complex'>
 print(c)
 
 print(c.real)
 print(c.imag)
 
+# we can perform operations
+print(c+d)
 
+print(type(e))
+print(e)
+print(e.real)
