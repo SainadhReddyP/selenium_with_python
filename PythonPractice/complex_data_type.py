66f8e910cfce701322cb76a52c843ef885ae2808
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 11:19:23 +0530

    complex data type

--- PythonPractice/complex_data_type.py
+++ PythonPractice/complex_data_type.py
@@ -1,6 +1,19 @@
 """
 Complex data type is used while developing scientific applications
 where complex mathematical operations are required.
+- syntax:
+real + imaginary j
+
+j - means -> square root of -1
+example:
+3+5j
+
 """
+c = 3+5j
+print(type(c))  # <class 'complex'>
+print(c)
+
+print(c.real)
+print(c.imag)
 
 
