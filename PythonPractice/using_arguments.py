a0d2c761b2c5f46777fd81298016be16ef03b031
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 10:52:54 +0530

    arguments in function-example

--- PythonPractice/using_arguments.py
+++ PythonPractice/using_arguments.py
@@ -3,3 +3,16 @@
 - sample_fun(*args)
 """
 
+
+def sample_function(*args):
+    for i in args:
+        print(i)
+
+
+def sample_function_two(a,b,c,d):
+    print(a,b,c,d)
+
+
+sample_function(1, 3, 5, 7, 9)
+args = [2,4,6,8]
+sample_function_two(*args)
