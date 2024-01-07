7feb007be2fb45dc71589e1807f3e68f02642585
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 11:07:28 +0530

    unpacking collections

---
+++ PythonPractice/unpacking_collections_into_individual_variables.py
@@ -0,0 +1,20 @@
+my_list = [1,3,5,7,9]
+
+# unpacking the list
+a, b, c, d, e = my_list
+print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+my_tuple = (2,4,6,8)
+
+f, g, h, i = my_tuple
+print(f)
+
+
+my_set = {11,12,13}
+
+j, k, l = my_set
+print(j)
