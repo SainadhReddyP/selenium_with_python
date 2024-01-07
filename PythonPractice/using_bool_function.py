8f1d4fa3928382d813449ac676850fd0ede86100
SainadhReddyP <meetsainadhreddy@gmail.com>
2024-01-07 22:47:24 +0530

    bool function

---
+++ PythonPractice/using_bool_function.py
@@ -0,0 +1,14 @@
+print(bool())  # False
+print(bool(5))  # True
+print(bool(""))  # False
+print(bool("Sainadh"))  # True
+
+print(bool([]))  # False
+print(bool([1,2,3]))  # True
+
+# Empty - False
+# Values - True
+
+
+
+
